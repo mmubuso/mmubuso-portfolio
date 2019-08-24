c040be6f30d7c78b12be092d2639a417d85f772e
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-24 18:50:24 -0400

    init files and create about me page

---
+++ src/containers/About Me/AboutMe.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './AboutMe.css'
+
+export default class AboutMe extends Component {
+
+    state ={
+        
+    }
+
+    render() {
+        return (
+            <div className='col-xs-12 container about-me'>
+                
+            </div>
+        )
+    }
+}
