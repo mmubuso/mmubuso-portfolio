0074a1baaee3aa8fa5c2a1868dfe23c3322f7296
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-08-25 19:11:45 -0400

    text to profile component

--- src/components/ProfilePicture/ProfilePicture.js
+++ src/components/ProfilePicture/ProfilePicture.js
@@ -3,11 +3,16 @@ import './ProfilePicture.css'
 
 export default function ProfilePicture() {
     return (
-        <div className='my-4 col-xs-8 profile-picture'>
-            <img
-                src={require('../../lib/profile-photo.jpg')}
-                alt='good looking african american male. wearing a stripped shirt'
-            />
+        <div className='my-4 col-xs-8'>
+            <div className='my-4 profile-picture'>
+                <img
+                    src={require('../../lib/profile-photo.jpg')}
+                    alt='good looking african american male. wearing a stripped shirt'
+                />
+            </div>
+            <div>
+                <h2 className='my-4'>Hello</h2>
+            </div>
         </div>
     )
 }
