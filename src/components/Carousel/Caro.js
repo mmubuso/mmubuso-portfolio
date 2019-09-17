8533e320efcd465ab29ee0ea5cd5a4588d735779
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-09-16 23:08:45 -0400

    update package

--- src/components/Carousel/Caro.js
+++ src/components/Carousel/Caro.js
@@ -9,79 +9,79 @@ const items = [
         header: 'Trillo'
     },
     {
-        src: '../images/chatter.gif',
+        src: './images/chatter.gif',
         altText: 'Slide 2',
         caption: 'messageing application',
         header: 'Chatter'
     },
     {
-        src: '../images/movie.gif',
+        src: './images/movie.gif',
         altText: 'Slide 3',
         caption: 'mobile app for looking up movies and epiosdes',
         header: 'React Native Movie App'
     },
     {
-        src: '../images/weathery2.gif',
+        src: './images/weathery2.gif',
         altText: 'Slide 4',
         caption: 'mobile app for looking up the weather',
         header: 'Weathery'
     },
     {
-        src: '../images/face-api.gif',
+        src: './images/face-api.gif',
         altText: 'Slide 5',
         caption: 'Capture facial expressions and show them as emojis',
         header: 'Face Recognition App'
     },
     {
-        src: '../images/sunrise.gif',
+        src: './images/sunrise.gif',
         altText: 'Slide 6',
         caption: 'sunrise alarm that will get brighter when the alarm goes off',
         header: 'Sunrise Alarm'
     },
     {
-        src: '../images/to-do-list.gif',
+        src: './images/to-do-list.gif',
         altText: 'Slide 7',
         caption: 'A To do list created with react native',
         header: 'To Do List'
     },
     {
-        src: '../images/calculator.gif',
+        src: './images/calculator.gif',
         altText: 'Slide 8',
         caption: 'A calculator built with react-native',
         header: 'Mobile Calculator'
     },
     {
-        src: '../images/image-filter.gif',
+        src: './images/image-filter.gif',
         altText: 'Slide 9',
         caption: 'An application for adjusting a pictures image filters',
         header: 'Image Filter'
     },
     {
-        src: '../images/photobooth.gif',
+        src: './images/photobooth.gif',
         altText: 'Slide 10',
         caption: 'A webcam photo booth ',
         header: 'Webcam Photobooth'
     },
     {
-        src: '../images/tip-calculator.gif',
+        src: './images/tip-calculator.gif',
         altText: 'Slide 11',
         caption: 'A mobile calculator for figuring out how much to tip',
         header: 'Tip calculator'
     },
     {
-        src: '../images/html-video.gif',
+        src: './images/html-video.gif',
         altText: 'Slide 12',
         caption: 'Custom video player created using vanilla javascript',
         header: 'HTML Video Player'
     },
     {
-        src: '../images/pomodoro-clock.gif',
+        src: './images/pomodoro-clock.gif',
         altText: 'Slide 13',
         caption: 'A timer to optomize effiency using the pomodoro technique',
         header: 'Pomodoro Timer'
     },
     {
-        src: '../images/simon.gif',
+        src: './images/simon.gif',
         altText: 'Slide 14',
         caption: 'A web version of the popular simon game',
         header: 'Simon Game'
