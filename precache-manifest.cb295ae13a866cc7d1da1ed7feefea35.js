a6a569b774b54a91c5c507350b11d42187f4fece
musiteli mubuso <mubuso.musiteli@gmail.com>
2019-09-16 22:59:59 -0400

    Updates

---
+++ precache-manifest.cb295ae13a866cc7d1da1ed7feefea35.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db994598e02cb352ffb1be697b20df7b",
+    "url": "/mmubuso-portfolio/index.html"
+  },
+  {
+    "revision": "8271e66569cac1e863a6",
+    "url": "/mmubuso-portfolio/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b752061d846db9dbd270",
+    "url": "/mmubuso-portfolio/static/css/main.f347dba8.chunk.css"
+  },
+  {
+    "revision": "8271e66569cac1e863a6",
+    "url": "/mmubuso-portfolio/static/js/2.91424256.chunk.js"
+  },
+  {
+    "revision": "b752061d846db9dbd270",
+    "url": "/mmubuso-portfolio/static/js/main.d938463f.chunk.js"
+  },
+  {
+    "revision": "851e54f85e94a7c22976",
+    "url": "/mmubuso-portfolio/static/js/runtime~main.82d75f9f.js"
+  },
+  {
+    "revision": "4d9e5a2040325df11d9757258a7c9ba5",
+    "url": "/mmubuso-portfolio/static/media/envelope.4d9e5a20.svg"
+  },
+  {
+    "revision": "3e49559ea819df68caad1171b7c6ca55",
+    "url": "/mmubuso-portfolio/static/media/facebook-black-social-button-circle.3e49559e.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/mmubuso-portfolio/static/media/github-logo.76d344f0.svg"
+  },
+  {
+    "revision": "9e36c8fd15d6f93ed6a45f9484746e8f",
+    "url": "/mmubuso-portfolio/static/media/linkedin.9e36c8fd.svg"
+  },
+  {
+    "revision": "8fdf734a92a6ab6b99d8bc31b6fd8abc",
+    "url": "/mmubuso-portfolio/static/media/location-sign.8fdf734a.svg"
+  },
+  {
+    "revision": "6cf8ef45f4297894f7b39e1fe36f8cce",
+    "url": "/mmubuso-portfolio/static/media/mobile-phone.6cf8ef45.svg"
+  },
+  {
+    "revision": "c28b656314fd73a7a05ceef92b63ed70",
+    "url": "/mmubuso-portfolio/static/media/profile-photo.c28b6563.jpg"
+  },
+  {
+    "revision": "8fe40281466fa95b8c6b1f9cfa0185fb",
+    "url": "/mmubuso-portfolio/static/media/twitter-logo-in-circular-black-button.8fe40281.svg"
+  }
+]);
